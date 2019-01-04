9547d21bcbd753c08afa74628113376efaa58331
kumarajeet024 <kumarajeet024@gmail.com>
2019-01-05 03:12:06 +0530

    request

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,12 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=f3cb19282c7c0636cccf91529010e64b'
+    city = 'New Delhi'
+    r = requests.get(url.format(city))
+    print(r.text)
     return render(request, 'weather/index.html')
 
