b3843eaf44e56d6d6e46163054301081505756da
kumarajeet024 <kumarajeet024@gmail.com>
2019-01-05 16:09:19 +0530

    request fetch

--- weather/views.py
+++ weather/views.py
@@ -6,7 +6,17 @@ from django.shortcuts import render
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=f3cb19282c7c0636cccf91529010e64b'
     city = 'New Delhi'
-    r = requests.get(url.format(city))
-    print(r.text)
-    return render(request, 'weather/index.html')
+    a = requests.get(url.format(city))
+    print(a.text)
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city' : city,
+        'temprature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+        'wind' : r['wind']['speed'],
+    }
+    context = {'city_weather':city_weather}
+    return render(request, 'weather/index.html', context)
 
